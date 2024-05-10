e8f730b6b505f860febee5aa628044e44c6ee41f
taeyeol <taeeyoul@gmail.com>
2024-05-11 00:59:22 +0900

    20240511

--- book/data_load.py
+++ book/data_load.py
@@ -2,20 +2,21 @@ import sqlite3
 import os
 
 # 환경 변수에서 데이터베이스 연결 정보 읽어오기
-db_url = os.environ.get('DB_URL')
-db_port = os.environ.get('DB_PORT')
-db_user_id = os.environ.get('DB_USER_ID')
-db_user_password = os.environ.get('DB_USER_PASSWORD')
+db_server        = os.environ.get('DB_SERVER')
+db_port          = os.environ.get('DB_PORT')
+database         = os.environ('DATABASE')
+db_user_id       = os.environ.get('DB_USERNAME')
+db_user_password = os.environ.get('DB_PASSWORD')
 
-# SQLite3 데이터베이스 연결
-conn = sqlite3.connect(f'{db_url}:{db_port}/{db_user_id}', db_user_password)
+# 데이터베이스 연결
+conn             = sqlite3.connect(f'{db_server}:{db_port}/{database}', db_user_id, db_user_password)
 
 # 커서 생성
-cursor = conn.cursor()
+cursor = conn.cursor( )
 
 # SQL 파일 읽기
-sql_file = open('tutorial_book.sql', 'r')
-sql_script = sql_file.read()
+sql_file   = open('tutorial_book.sql', 'r')
+sql_script = sql_file.read( )
 sql_file.close()
 
 # SQL 스크립트 실행
